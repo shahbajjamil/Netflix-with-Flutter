904da53c5da4fd3cd15edeee4577ac9fa5330ab5
shahbajjamil <shahbajjamil2014@gmail.com>
2021-01-01 20:53:44 +0530

    web view added

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/models/models.dart';
 import 'package:flutter_netflix_responsive_ui/widgets/widgets.dart';
+import 'package:video_player/video_player.dart';
 
 class ContentHeader extends StatelessWidget {
   final Content featuredContent;
@@ -9,6 +10,23 @@ class ContentHeader extends StatelessWidget {
     Key key,
     @required this.featuredContent,
   }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: _ContentHeaderMobile(featuredContent: featuredContent),
+      desktop: _ContentHeaderDesktop(featuredContent: featuredContent),
+    );
+  }
+}
+
+class _ContentHeaderMobile extends StatelessWidget {
+  final Content featuredContent;
+
+  const _ContentHeaderMobile({
+    Key key,
+    this.featuredContent,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -66,11 +84,99 @@ class ContentHeader extends StatelessWidget {
   }
 }
 
+class _ContentHeaderDesktop extends StatefulWidget {
+  final Content featuredContent;
+
+  const _ContentHeaderDesktop({
+    Key key,
+    this.featuredContent,
+  }) : super(key: key);
+
+  @override
+  __ContentHeaderDesktopState createState() => __ContentHeaderDesktopState();
+}
+
+class __ContentHeaderDesktopState extends State<_ContentHeaderDesktop> {
+  VideoPlayerController _videoController;
+  bool _isMuted = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoController =
+        VideoPlayerController.network(widget.featuredContent.videoUrl)
+          ..initialize().then((_) => setState(() {}))
+          ..play();
+  }
+
+  @override
+  void dispose() {
+    _videoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          height: 500.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(widget.featuredContent.imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          height: 500.0,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.transparent],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 110.0,
+          child: SizedBox(
+            width: 250.0,
+            child: Image.asset(widget.featuredContent.titleImageUrl),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 40.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              VerticalIconButton(
+                icon: Icons.add,
+                title: 'List',
+                onTap: () => print("My List"),
+              ),
+              _PlayButton(),
+              VerticalIconButton(
+                icon: Icons.info_outline,
+                title: 'Info',
+                onTap: () => print("Info"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class _PlayButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlatButton.icon(
-      padding: const EdgeInsets.fromLTRB(15.0, 5.0, 20.0, 5.0 ),
+      padding: const EdgeInsets.fromLTRB(15.0, 5.0, 20.0, 5.0),
       onPressed: () => print("play"),
       color: Colors.white,
       icon: const Icon(
