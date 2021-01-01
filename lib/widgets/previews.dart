ac57e5f148fc257ddb33004634dabd03c1d2e493
shahbajjamil <shahbajjamil2014@gmail.com>
2021-01-01 19:07:54 +0530

    complete home page

--- lib/widgets/previews.dart
+++ lib/widgets/previews.dart
@@ -20,9 +20,75 @@ class Previews extends StatelessWidget {
           child: Text(
             title,
             style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold),
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          height: 165.0,
+          child: ListView.builder(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            scrollDirection: Axis.horizontal,
+            itemCount: contentList.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Content content = contentList[index];
+              return GestureDetector(
+                onTap: () => print(content.name),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 130.0,
+                      width: 130.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(content.imageUrl),
+                          fit: BoxFit.cover,
+                        ),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: content.color,
+                          width: 4.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 130.0,
+                      width: 130.0,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Colors.black87,
+                            Colors.black45,
+                            Colors.transparent,
+                          ],
+                          stops: [0, 0.25, 1],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: content.color,
+                          width: 4.0,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        child: SizedBox(
+                          height: 60.0,
+                          child: Image.asset(content.titleImageUrl),
+                        ))
+                  ],
+                ),
+              );
+            },
           ),
         )
       ],
