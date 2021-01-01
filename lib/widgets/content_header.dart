3e0672588eddcd373f91527d4d1396b32fc72181
shahbajjamil <shahbajjamil2014@gmail.com>
2021-01-02 00:38:48 +0530

    home page done for both web and app

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -89,7 +89,7 @@ class _ContentHeaderDesktop extends StatefulWidget {
 
   const _ContentHeaderDesktop({
     Key key,
-    this.featuredContent,
+    @required this.featuredContent,
   }) : super(key: key);
 
   @override
@@ -106,6 +106,7 @@ class __ContentHeaderDesktopState extends State<_ContentHeaderDesktop> {
     _videoController =
         VideoPlayerController.network(widget.featuredContent.videoUrl)
           ..initialize().then((_) => setState(() {}))
+          ..setVolume(0)
           ..play();
   }
 
@@ -117,57 +118,123 @@ class __ContentHeaderDesktopState extends State<_ContentHeaderDesktop> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          height: 500.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(widget.featuredContent.imageUrl),
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => _videoController.value.isPlaying
+          ? _videoController.pause()
+          : _videoController.play(),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          AspectRatio(
+            aspectRatio: _videoController.value.initialized
+                ? _videoController.value.aspectRatio
+                : 2.344,
+            child: _videoController.value.initialized
+                ? VideoPlayer(_videoController)
+                : Image.asset(
+                    widget.featuredContent.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: -1.0,
+            child: AspectRatio(
+              aspectRatio: _videoController.value.initialized
+                  ? _videoController.value.aspectRatio
+                  : 2.344,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.black, Colors.transparent],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
-        Container(
-          height: 500.0,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.black, Colors.transparent],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
+          Container(
+            height: 500.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.black, Colors.transparent],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              ),
             ),
           ),
-        ),
-        Positioned(
-          bottom: 110.0,
-          child: SizedBox(
-            width: 250.0,
-            child: Image.asset(widget.featuredContent.titleImageUrl),
+          Positioned(
+            left: 60,
+            right: 60,
+            bottom: 140.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 250.0,
+                  child: Image.asset(widget.featuredContent.titleImageUrl),
+                ),
+                const SizedBox(height: 15.0),
+                Text(
+                  widget.featuredContent.description,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        offset: Offset(2.0, 4.0),
+                        blurRadius: 6.0,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    _PlayButton(),
+                    const SizedBox(width: 16.0),
+                    FlatButton.icon(
+                      padding:
+                          const EdgeInsets.fromLTRB(25.0, 10.0, 30.0, 10.0),
+                      onPressed: () => print("More Info"),
+                      color: Colors.white,
+                      icon: const Icon(Icons.info_outline, size: 30.0),
+                      label: const Text(
+                        "More Info",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20.0),
+                    if (_videoController.value.initialized)
+                      IconButton(
+                        icon: Icon(
+                          _isMuted ? Icons.volume_off : Icons.volume_up,
+                        ),
+                        color: Colors.white,
+                        iconSize: 30.0,
+                        onPressed: () => setState(
+                          () {
+                            _isMuted
+                                ? _videoController.setVolume(100)
+                                : _videoController.setVolume(0);
+                            _isMuted = _videoController.value.volume == 0;
+                          },
+                        ),
+                      ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          bottom: 40.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              VerticalIconButton(
-                icon: Icons.add,
-                title: 'List',
-                onTap: () => print("My List"),
-              ),
-              _PlayButton(),
-              VerticalIconButton(
-                icon: Icons.info_outline,
-                title: 'Info',
-                onTap: () => print("Info"),
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
@@ -176,7 +243,9 @@ class _PlayButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlatButton.icon(
-      padding: const EdgeInsets.fromLTRB(15.0, 5.0, 20.0, 5.0),
+      padding: !Responsive.isdesktop(context)
+          ? const EdgeInsets.fromLTRB(15.0, 5.0, 20.0, 5.0)
+          : const EdgeInsets.fromLTRB(25.0, 10.0, 30.0, 10.0),
       onPressed: () => print("play"),
       color: Colors.white,
       icon: const Icon(
